67ac7d608c12277e20dcd984bcf2f8fff5c59733
Gianni <gricciardi@mobydigital.com>
2021-10-13 01:05:00 -0300

    Se creó la entidad User

---
+++ Models/Student.php
@@ -0,0 +1,65 @@
+<?php
+namespace Model;
+
+    class Student extends User {
+
+        private $firstName; //String
+        private $lastName; //String
+        private $dni; //string
+        private $id; //string
+
+        //Averiguar cómo hacemos para pasarle por parámetro el id de User, siendo clase padre de Student, que también tiene su propio id.
+        public function __construct($firstName, $lastName, $dni, $id, $userName, $password, $email){
+
+        }
+
+        
+        public function getFirstName() {
+                return $this->firstName;
+        }
+
+      
+        public function setFirstName($firstName){
+                $this->firstName = $firstName;
+
+                return $this;
+        }
+
+        
+        public function getLastName() {
+                return $this->lastName;
+        }
+
+        
+        public function setLastName($lastName) {
+                $this->lastName = $lastName;
+
+                return $this;
+        }
+
+        public function getDni(){
+                return $this->dni;
+        }
+
+       
+        public function setDni($dni){
+                $this->dni = $dni;
+
+                return $this;
+        }
+
+       
+        public function getId() {
+                return $this->id;
+        }
+
+        
+        public function setId($id){
+                $this->id = $id;
+
+                return $this;
+        }
+    }
+
+
+?>
