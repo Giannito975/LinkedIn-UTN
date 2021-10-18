0a608bf8420bcbcaa0c4b82210cc027c2192784e
Gianni <gricciardi@mobydigital.com>
2021-10-18 20:38:27 -0300

    Creada plantilla HTML formulario de ingreso

---
+++ Linkedin UTN/Views/login.php
@@ -0,0 +1,20 @@
+<div class="login">
+    <form action="loginProcessor.php" method="POST">
+        <div class="form-group">
+            <div class="form-control">
+                <label for="username">User name</label>
+                <input type="text" name="username" placeholder="Type your username or email" required>
+            </div>
+            <div class="form-control">
+                <label for="password">Password</label>
+                <input type="password" name="password" placeholder="Type your password" required>
+            </div>
+            <div class="input-btn">
+                <button type="submit">Ingresar</button>
+            </div>
+        </div>
+        <div class="forgot-password">
+            <a href="">Forgot your password?</a>
+        </div>
+    </form>
+</div>
