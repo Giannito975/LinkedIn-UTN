d5c9a3203784cd10d5214b8976cfcec30db18e29
DanteGrassiHait <31975704+DanteGrassiHait@users.noreply.github.com>
2021-10-24 15:59:58 -0300

    Agregadas funciones CRUD

--- DAO/StudentDAO.php
+++ DAO/StudentDAO.php
@@ -2,29 +2,28 @@
     namespace DAO;
 
     use \Exception as Exception;
-    use DAO\IStudentDAO as IStudentDAO;
+    use DAO\IDao as IDao;
     use Models\Student as Student;    
     use DAO\Connection as Connection;
 
-    class StudentDAO implements IStudentDAO
+    class StudentDAO implements IDao
     {
         private $connection;
         private $tableName = "students";
 
-        public function Add(Student $student)
+        public function Add($student)
         {
             try
             {
-                $query = "INSERT INTO ".$this->tableName." (recordId, firstName, lastName) VALUES (:recordId, :firstName, :lastName);";
-                
-                $parameters["recordId"] = $student->getRecordId();
-                $parameters["firstName"] = $student->getFirstName();
-                $parameters["lastName"] = $student->getLastName();
+                $query = "CALL Student_Add(?, ?, ?)";//Se guarda la accion que se hara en la BDD
+
+                $parameters["student_firstname"] =  $student->getFirstName();
+                $parameters["student_lastname"] = $student->getLastName();
 
                 $this->connection = Connection::GetInstance();
 
-                $this->connection->ExecuteNonQuery($query, $parameters);
-            }
+                $this->connection->ExecuteNonQuery($query, $parameters, QueryType::StoredProcedure);//Realiza la llamada a la funcion en la BDD
+            }   
             catch(Exception $ex)
             {
                 throw $ex;
@@ -37,24 +36,88 @@
             {
                 $studentList = array();
 
-                $query = "SELECT * FROM ".$this->tableName;
+                $query = "CALL Student_GetAll()";//Se guarda la accion que se hara en la BDD
 
                 $this->connection = Connection::GetInstance();
 
-                $resultSet = $this->connection->Execute($query);
-                
-                foreach ($resultSet as $row)
-                {                
-                    $student = new Student();
-                    $student->setRecordId($row["recordId"]);
-                    $student->setFirstName($row["firstName"]);
-                    $student->setLastName($row["lastName"]);
+                $result = $this->connection->Execute($query, array(), QueryType::StoredProcedure);//Realiza la llamada a la funcion y se guarda lo que devuelve la funcion de la BDD
+
+                foreach($result as $row)
+                {
+                    $student = new student();
+                    $student->setId($row["id_student"]);
+                    $student->setFirstName($row["student_firstname"]);
+                    $student->setLastName($row["student_lastname"]);
 
                     array_push($studentList, $student);
                 }
-
                 return $studentList;
+            }   
+            catch(Exception $ex)
+            {
+                throw $ex;
             }
+        }
+
+        public function GetById($id)
+        {
+            try
+            {
+
+                $query = "CALL Student_GetById(?)";//Se guarda la accion que se hara en la BDD
+
+                $parameters["id_student"] =  $id;
+
+                $this->connection = Connection::GetInstance();
+ 
+                $result = $this->connection->Execute($query, $parameters, QueryType::StoredProcedure);//Realiza la llamada a la funcion y se guarda lo que devuelve la funcion de la BDD
+                
+                $student = new Student();
+                $student->setId($result[0]["id_student"]);
+                $student->setFirstName($result[0]["student_firstname"]);
+                $student->setLastName($result[0]["student_lastname"]);
+                
+                return $student;
+
+            }   
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function Update($student)
+        {
+            try
+            {
+                $query = "CALL Student_Update(?, ?, ?)";//Se guarda la accion que se hara en la BDD
+
+                $parameters["id_student"] =  $student->getId();
+                $parameters["student_firstname"] =  $student->getFirstName();
+                $parameters["student_lastname"] = $student->getLastName();
+                
+                $this->connection = Connection::GetInstance();
+
+                $this->connection->ExecuteNonQuery($query, $parameters, QueryType::StoredProcedure);//Realiza la llamada a la funcion
+            }   
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function Remove($id)
+        {
+            try
+            {
+                $query = "CALL Student_Remove(?)";//Se guarda la accion que se hara en la BDD
+
+                $parameters["id_student"] = $id;
+
+                $this->connection = Connection::GetInstance();
+
+                echo $this->connection->ExecuteNonQuery($query, $parameters, QueryType::StoredProcedure);//Realiza la llamada a la funcion en la BDD
+            }   
             catch(Exception $ex)
             {
                 throw $ex;
