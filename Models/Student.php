f085b917ad400beee131fdc1623539d3e94bbc9a
DanteGrassiHait <31975704+DanteGrassiHait@users.noreply.github.com>
2021-10-28 22:06:42 -0300

    Actualizada la clase Student

--- Models/Student.php
+++ Models/Student.php
@@ -9,32 +9,54 @@ namespace Models;
         private $fileNumber; //string
         private $gender; //genero
         private $birthDate; //atributo tipo Date (revisar)
-        private $email;
         private $phoneNumber;
-        private $isActive; //boolean
-        private $id; //string
-        private $role; //integer: 0 = admin, 1 = student, 2 = empresa.
+        private $career; 
+        private $idStudent; //string
 
-        //Averiguar cómo hacemos para pasarle por parámetro el id de User, siendo clase padre de Student, que también tiene su propio id.
-        public function __construct($firstName, $lastName, $dni, $id, $userName, $password, $email){
-
+        public function getCareer() {
+                return $this->career;
         }
 
+      
+        public function setCareer($career){
+                $this->career = $career;
+        }
+
+        public function getPhoneNumber() {
+                return $this->phoneNumber;
+        }
+      
+        public function setPhoneNumber($phoneNumber){
+                $this->phoneNumber = $phoneNumber;
+        }        
+        
+        public function getBirthDate() {
+                return $this->birthDate;
+        }
+      
+        public function setBirthDate($birthDate){
+                $this->birthDate = $birthDate;
+        }
+
+        public function getGender() {
+                return $this->gender;
+        }
+      
+        public function setGender($gender){
+                $this->gender = $gender;
+        }
         
         public function getFirstName() {
                 return $this->firstName;
         }
-
       
         public function setFirstName($firstName){
                 $this->firstName = $firstName;
         }
-
         
         public function getLastName() {
                 return $this->lastName;
         }
-
         
         public function setLastName($lastName) {
                 $this->lastName = $lastName;
@@ -43,25 +65,19 @@ namespace Models;
         public function getDni(){
                 return $this->dni;
         }
-
        
         public function setDni($dni){
                 $this->dni = $dni;
         }
-
        
-        public function getId() {
-                return $this->id;
+        public function getidStudent() {
+                return $this->idStudent;
         }
-
         
-        public function setId($id){
-                $this->id = $id;
+        public function setidStudent($idStudent){
+                $this->idStudent = $idStudent;
         }
-
-        
-
-       
+               
         public function getFileNumber() {
                 return $this->fileNumber;
         }
