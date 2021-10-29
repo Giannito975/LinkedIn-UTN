3d986546d80983b120b167388d548255d0fbf818
DanteGrassiHait <31975704+DanteGrassiHait@users.noreply.github.com>
2021-10-28 22:10:54 -0300

    Actualizada la clase Career

--- Models/Career.php
+++ Models/Career.php
@@ -4,47 +4,33 @@ namespace Models;
 
     class Career {
 
-        private $id;
-        private $name;
-        private $title; //título que se entrega una vez completada
-        private $status;
+        private $idCareer;
+        private $description;
+        private $active;
         
         
-        public function getId(){
-                return $this->id;
+        public function getIdCareer(){
+                return $this->idCareer;
         }
 
-        public function setId($id) {
-                $this->id = $id;
+        public function setIdCareer($idCareer){
+                $this->idCareer = $idCareer;
         }
-
         
-        public function getName() {
-                return $this->name;
+        public function getDescription() {
+                return $this->description;
         }
 
-        public function setName($name){
-                $this->name = $name;
+        public function setDescription($description){
+                $this->description = $description;
         }
-
         
-        public function getTitle() {
-                return $this->title;
+        public function getActive() {
+                return $this->active;
         }
-
       
-        public function setTitle($title)
-        {         $this->title = $title;
-        }
-
-        
-        public function getStatus() {
-                return $this->status;
-        }
-
-       
-        public function setStatus($status) {
-                $this->status = $status;
+        public function setActive($active){         
+                $this->active = $active;
         }
     }
 
