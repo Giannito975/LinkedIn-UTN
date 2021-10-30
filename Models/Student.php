abb0f5e7923e0ac9e0459f4c28034e30eeca3e70
DanteGrassiHait <31975704+DanteGrassiHait@users.noreply.github.com>
2021-10-30 16:08:25 -0300

    Add y GetAll de Students

--- Models/Student.php
+++ Models/Student.php
@@ -12,8 +12,9 @@ class Student extends User {
     private $phoneNumber;
     private $idCareer; 
     private $idStudent; //string
+    private $active;
 
-    public function __construct($idStudent, $idCareer, $firstName, $lastName, $dni, $fileNumber, $gender, $birthdate, $phoneNumber)
+    public function __construct($idStudent, $idCareer, $firstName, $lastName, $dni, $fileNumber, $gender, $birthdate, $phoneNumber, $active)
     {
             $this->idStudent = $idStudent;
             $this->idCareer = $idCareer;
@@ -24,80 +25,90 @@ class Student extends User {
             $this->gender = $gender;
             $this->birthDate = $birthdate;
             $this->phoneNumber = $phoneNumber;
+            $this->active = $active;
     }
 
-    public function getIdCareer() {
-            return $this->idCareer;
-    }
+        public function getActive() {
+                return $this->active;
+        }
+
+
+        public function setActive($active){
+                $this->active = $active;
+        }
+
+        public function getIdCareer() {
+                return $this->idCareer;
+        }
 
   
-    public function setIdCareer($idCareer){
-            $this->idCareer = $idCareer;
-    }
+        public function setIdCareer($idCareer){
+                $this->idCareer = $idCareer;
+        }
 
-    public function getPhoneNumber() {
-            return $this->phoneNumber;
-    }
-  
-    public function setPhoneNumber($phoneNumber){
-            $this->phoneNumber = $phoneNumber;
-    }        
-    
-    public function getBirthDate() {
-            return $this->birthDate;
-    }
-  
-    public function setBirthDate($birthDate){
-            $this->birthDate = $birthDate;
-    }
+        public function getPhoneNumber() {
+                return $this->phoneNumber;
+        }
+        
+        public function setPhoneNumber($phoneNumber){
+                $this->phoneNumber = $phoneNumber;
+        }        
+        
+        public function getBirthDate() {
+                return $this->birthDate;
+        }
+        
+        public function setBirthDate($birthDate){
+                $this->birthDate = $birthDate;
+        }
 
-    public function getGender() {
-            return $this->gender;
-    }
-  
-    public function setGender($gender){
-            $this->gender = $gender;
-    }
-    
-    public function getFirstName() {
-            return $this->firstName;
-    }
-  
-    public function setFirstName($firstName){
-            $this->firstName = $firstName;
-    }
-    
-    public function getLastName() {
-            return $this->lastName;
-    }
-    
-    public function setLastName($lastName) {
-            $this->lastName = $lastName;
-    }
+        public function getGender() {
+                return $this->gender;
+        }
+        
+        public function setGender($gender){
+                $this->gender = $gender;
+        }
+        
+        public function getFirstName() {
+                return $this->firstName;
+        }
+        
+        public function setFirstName($firstName){
+                $this->firstName = $firstName;
+        }
+        
+        public function getLastName() {
+                return $this->lastName;
+        }
+        
+        public function setLastName($lastName) {
+                $this->lastName = $lastName;
+        }
 
-    public function getDni(){
-            return $this->dni;
-    }
-   
-    public function setDni($dni){
-            $this->dni = $dni;
-    }
-   
-    public function getIdStudent() {
-            return $this->idStudent;
-    }
-    
-    public function setIdStudent($idStudent){
-            $this->idStudent = $idStudent;
-    }
-           
-    public function getFileNumber() {
-            return $this->fileNumber;
-    }
+        public function getDni(){
+                return $this->dni;
+        }
+        
+        public function setDni($dni){
+                $this->dni = $dni;
+        }
+        
+        public function getIdStudent() {
+                return $this->idStudent;
+        }
+        
+        public function setIdStudent($idStudent){
+                $this->idStudent = $idStudent;
+        }
+                
+        public function getFileNumber() {
+                return $this->fileNumber;
+        }
 
-    public function setFileNumber($fileNumber) {
-            $this->fileNumber = $fileNumber;
-    }
+        public function setFileNumber($fileNumber) {
+                $this->fileNumber = $fileNumber;
+        }
 }
 
 
