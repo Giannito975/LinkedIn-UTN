4b8793c5b8ace09bb480566f5fc7b6a2c1bb7806
DanteGrassiHait <31975704+DanteGrassiHait@users.noreply.github.com>
2021-10-31 19:26:19 -0300

    ABM CompanyDao

--- DAO/CompanyDao.php
+++ DAO/CompanyDao.php
@@ -2,6 +2,7 @@
 
     namespace DAO;
 
+use Exception;
 use Models\Company;
 
 class CompanyDao{
@@ -63,6 +64,77 @@ class CompanyDao{
                 return $this->companyList;
             }
             catch (\PDOException $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function GetById($id){
+            try
+            {
+                $query = "SELECT * FROM ".$this->tableName." WHERE id_company = '".$id."'";//Se guarda la accion que se hara en la BDD
+
+                $this->connection = Connection::GetInstance();
+    
+                $result = $this->connection->Execute($query, array());//Realiza la llamada a la funcion y se guarda lo que devuelve la funcion de la BDD
+                    
+                foreach($result as $row){
+                    $company = new Company(
+                        $row['id_company'], 
+                        $row['name'], 
+                        $row['about_us'], 
+                        $row["company_link"],
+                        $row['email'], 
+                        $row['industry'], 
+                        $row["city"],
+                        $row["country"]
+                        );
+                }
+                var_dump($company);
+                return $company;
+            }   
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function UpdateCompany(Company $company){
+            try
+            {
+                $query= "UPDATE ".$this->tableName." SET name = :name, about_us = :about_us, company_link = :company_link, email = :email, industry = :industry, city = :city, country = :country 
+                WHERE (id_company = :id_company)";
+
+                $parameters['name'] = $company->getName();
+                $parameters['about_us'] = $company->getAbout_us();
+                $parameters['company_link'] = $company->getCompany_link();
+                $parameters['email'] = $company->getEmail();
+                $parameters['industry'] = $company->getIndustry();
+                $parameters['city'] = $company->getCity();
+                $parameters['country'] = $company->getCountry();
+
+                $this->connection = Connection::GetInstance();
+
+                $this->connection->ExecuteNonQuery($query, $parameters);
+            }
+            catch(\PDOException $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function DeleteCompany($id_company){
+            try
+            {
+                $query = "DELETE FROM ".$this->tableName." WHERE (id_company = :id_company)";
+
+                $parameters["id_company"] =  $id_company;
+
+                $this->connection = Connection::GetInstance();
+
+                return $count=$this->connection->ExecuteNonQuery($query, $parameters);
+            }
+            catch(\PDOException $ex)
             {
                 throw $ex;
             }
