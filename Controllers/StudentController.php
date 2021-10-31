8b52135423112b49c149569fb2151786f55757a0
DanteGrassiHait <31975704+DanteGrassiHait@users.noreply.github.com>
2021-10-31 14:45:12 -0300

    Agregadas verificaciones y Add

--- Controllers/StudentController.php
+++ Controllers/StudentController.php
@@ -20,11 +20,24 @@
 
         public function ShowListView()
         {
+            $this->Add();
             //$studentList = $this->studentDAO->retrieveStudentsJson();
             //$studentList = $this->studentDAO->GetAll();
             //$studentList = $this->studentDAO->GetByEmail("dhasely4@blinklist.com");
 
             require_once(VIEWS_PATH."home.php");
+        }
+
+        public function Add(){
+            $studentList = $this->studentDAO->retrieveStudentsJson();
+            if($this->verifyGetAll()){
+                foreach($studentList as $student){
+                    if(!$this->verifyId($student->getIdStudent())){
+                        $this->studentDAO->Add($student);
+                    }
+                }
+            }
+
         }
 
         public function GetAll(){
@@ -68,9 +81,12 @@
 
         public function GetByEmail($email){
             try{
-                $student = $this->studentDAO->GetByEmail($email);
-                if($student->getEmail() == $email){
-                    return true;
+                if($this->verifyEmail($email)){
+                    $student = $this->studentDAO->GetByEmail($email);
+                    if($student->getEmail() == $email){
+                        return true;
+                    }
+                    return false;
                 }
                 return false;
             }
@@ -78,6 +94,45 @@
                 $message = $e->getMessage();
                 $this->homeController->StudentView($message);
             }
+        }
+
+        public function verifyGetAll(){
+            if(!empty($this->GetAll())){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public function verifyEmail($email){
+            $studentList = $this->GetAll();
+            foreach($studentList as $student){
+                if($student->getEmail() == $email){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public function verifyPassword($password){
+            $studentList = $this->GetAll();
+            foreach($studentList as $student){
+                if($student->getPassword() == $password){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public function verifyId($id){
+            $studentList = $this->GetAll();
+            foreach($studentList as $student){
+                if($student->getIdStudent() == $id){
+                    return true;
+                }
+            }
+            return false;
         }
         
     }
