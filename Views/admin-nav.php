fc999ee85dd505c6abbb89ba99d85e196ca91d4c
Gianni <gricciardi@mobydigital.com>
2021-11-03 00:22:58 -0300

    Modificacion enrutamiento nav admin

--- Views/admin-nav.php
+++ Views/admin-nav.php
@@ -3,9 +3,9 @@
 ?>
 <ul class="nav justify-content-center">
   <li class="nav-item">
-    <a class="nav-link" href="<?php echo FRONT_ROOT."Home/StudentListView"?>">Listado empresas</a>
+    <a class="nav-link" href="<?php echo FRONT_ROOT."Home/CompanyListViewAdmin"?>">Listado empresas</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="<?php echo FRONT_ROOT."Home/CompanyListView"?>">Listado ofertas laborales</a>
+    <a class="nav-link" href="<?php echo FRONT_ROOT."Admin/ShowJobOfferViewAdmin"?>">Listado ofertas laborales</a>
   </li>
 </ul>
