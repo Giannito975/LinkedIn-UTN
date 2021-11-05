af58ca5e2ce177e7ef1152ba8af3def4e377ca6a
Gianni <gricciardi@mobydigital.com>
2021-11-05 01:29:54 -0300

    Se modificó el listado de companies desde el admin, estilos nuevos

--- Views/company-list-admin.php
+++ Views/company-list-admin.php
@@ -1,36 +1,177 @@
-<div class="company-list-admin">
-    <?php
+<?php
         require_once('header.php');
         require_once('admin-nav.php');
     ?>
-    <div class="company-container">
-        <table class="table bg-light-alpha">
-            <thead>
-                <th>Name</th>
-                <th>About Us</th>
-                <th>Company Link</th>
-                <th>Email</th>
-                <th>Industry</th>
-                <th>City</th>
-                <th>Country</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <?php foreach($companyList as $company){?>
-                    <td><?php echo $company->getName(); ?></td>
-                    <td><?php echo $company->getAbout_us(); ?></td>
-                    <td><?php echo $company->getCompany_link(); ?></td>
-                    <td><?php echo $company->getEmail(); ?></td>
-                    <td><?php echo $company->getIndustry(); ?></td>
-                    <td><?php echo $company->getCity(); ?></td>
-                    <td><?php echo $company->getCountry(); ?></td>
-                    <td><a href="">Modificar</a></td>
-                    <td><a href="">Eliminar</a></td>
-                </tr>
-                <tr>
-                    <?php } ?>
-                </tr>
-            </tbody>
-        </table>
+<main class="p-5">
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        <!-- Page Heading 
+        <h1 class="h3 mb-2 text-gray-800"> <?php //echo "$validMessage"; ?> </h1>-->
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Company list</h6>
+            </div>
+
+            <div class="card-body">
+                <div class="table-responsive">
+
+                    <div class="form-group mb-4">
+                        <button type="button" class="btn btn-success mr-4" data-toggle="modal" data-target="#form-cine">
+                            <img src="<?php echo ICONS_PATH."plus.svg"?>" width="16" height="16" alt="Add" /> Add
+                            Company
+                        </button>
+                    </div>
+
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Adress</th>
+
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <?php   foreach($companyList as $company){   ?>
+
+                            <tr id="row-<?php echo $company->getId_company(); ?>">
+
+                                <td><?php echo $company->getName(); ?></td>
+                                <td><?php echo $company->getAbout_us(); ?></td>
+                                <td><?php echo $company->getCompany_link(); ?></td>
+                                <td><?php echo $company->getEmail(); ?></td>
+                                <td><?php echo $company->getIndustry(); ?></td>
+                                <td><?php echo $company->getCity(); ?></td>
+                                <td><?php echo $company->getCountry(); ?></td>
+
+                                <td>
+                                    <div class="form-inline">
+                                        <form action="<?php echo FRONT_ROOT."Company/Remove"?>" method="POST">
+                                            <button type="submit" name="remove" class="btn btn-danger"
+                                                value="<?php echo $company->getId_company(); ?>">
+                                                <img src="<?php echo ICONS_PATH."trash-2.svg"?>" width="16" height="16"
+                                                    alt="Remove" />
+                                            </button>
+                                        </form>
+
+
+                                        <button class="btn btn--edit btn-info ml-4 "
+                                            data-id="<?php echo $company->getId_company(); ?>" data-toggle="modal"
+                                            data-target="#form-cine">
+                                            <img src="<?php echo ICONS_PATH."edit.svg"?>" width="16" height="16"
+                                                alt="Update" />
+                                        </button>
+                                    </div>
+                                </td>
+
+                            </tr>
+                            <?php   } ?>
+
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+</main>
+
+<!-- add company -->
+<div class="modal fade" id="form-cine" tabindex="-1" role="dialog" aria-labelledby="sign-up" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+
+        <form class="modal-content" action="<?php echo FRONT_ROOT ?>Company/Middleware" method="POST">
+
+            <div class="modal-header">
+                <h5 class="modal-title">Add Company</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+
+                <div class="form-group">
+                    <label>Nombre</label>
+                    <input pattern="[a-zA-Z\s]+" title="Please enter on alphabets only" type="text" class="form-control"
+                        name="name" required />
+                </div>
+
+                <div class="form-group">
+                    <label>Direccion</label>
+                    <input pattern="[a-zA-Z0-9\s]+" title="Please enter on alphabets only" type="text"
+                        class="form-control" name="adress" required />
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-link" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-dark">Add</button>
+            </div>
+
+            <input type="hidden" name="id" value="" />
+
+        </form>
+
     </div>
 </div>
+
+<?php if(isset($message)) { ?>
+<div id="message-toast" class="toast showing bg-danger" role="alert" aria-live="assertive" aria-atomic="true"
+    style="position:fixed;bottom:0;right:0; min-height:100px; z-index:10000">
+    <div class="toast-header bg-danger text-white border-bottom-0">
+        <strong class="mr-auto">LinkedIn UTN</strong>
+        <button type="button" class="ml-2 mb-1 close text-white" id="btn-close-toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="toast-body bg-danger text-white">
+        <?php echo $message; ?>
+    </div>
+</div>
+<?php } ?>
+
+<script type="text/javascript">
+function init($) {
+    $('#form-cine').on('hide.bs.modal', function(e) {
+        $("#form-cine .form-control[name='name']").val('')
+        $("#form-cine .form-control[name='adress']").val('')
+        $("#form-cine .form-control[name='name']").attr('readonly', false)
+        $('#form-cine .modal-title').text("Add Cine");
+        $('#form-cine .modal-footer > button[type=submit]').text("Add")
+    })
+    $(".btn--edit").click(function() {
+        $('#form-cine .modal-title').text("Update Cinema");
+        $('#form-cine .modal-footer > button[type=submit]').text("Update")
+        var cine = {
+            id: $(this).data("id")
+        };
+        var containerQuery = "#row-" + cine.id;
+        var cells = $(containerQuery + '> td')
+        for (var i = 0; i < cells.length; i++) {
+            switch (i) {
+                case 0:
+                    cine.name = $(cells[i]).text();
+                    break;
+                case 1:
+                    cine.adress = $(cells[i]).text();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        for (var attr in cine) {
+            var element = $("#form-cine input[name='" + attr + "']");
+            element.val(cine[attr]);
+        }
+    })
+}
+</script>
