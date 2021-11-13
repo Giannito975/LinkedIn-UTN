02686b6dfb6abf2ea142655e852d3a396a877b09
Gianni <gianniricciardi@3779@gmail.com>
2021-11-13 11:22:56 -0300

    Se corrigieron redireccionamientos del user

--- Views/student-profile.php
+++ Views/student-profile.php
@@ -11,7 +11,9 @@
         require_once('student-nav.php');
     ?>
     <div class="go-back-btn">
-        <a href="<?php FRONT_ROOT."Student/ShowJobOfferView"?>">Go back</a>
+    <a href="<?php echo FRONT_ROOT."JobOffer/JobOfferListViewStudent" ?>">
+        Go Back
+    </a>
     </div>
     <!-- foreach con todas las companys -->
     <div class="company-container">
