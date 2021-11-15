75a0226d31aa91bd8ad31a29cb6af749d3c25e1b
Gianni <gianniricciardi@3779@gmail.com>
2021-11-15 15:47:47 -0300

    realizada vista de historial aplicaciones de un estudiante

--- Views/student-profile.php
+++ Views/student-profile.php
@@ -4,18 +4,25 @@
         }
 ?>
 <div class="company-list">
-    
-    
+
+
     <?php
         require_once('header.php');
         require_once('student-nav.php');
     ?>
     <div class="go-back-btn">
-    <a href="<?php echo FRONT_ROOT."JobOffer/JobOfferListViewStudent" ?>">
-        Go Back
-    </a>
+        <a href="<?php echo FRONT_ROOT."JobOffer/JobOfferListViewStudent" ?>">
+            Go Back
+        </a>
     </div>
     <!-- foreach con todas las companys -->
+    <br>
+    <a href="<?php echo FRONT_ROOT."JobOffer/JobOfferListViewStudent" ?>">
+        <button type="submit" class="btn btn-success mr-4" data-toggle="modal" 
+           >
+            <img src="<?php echo ICONS_PATH."user-check.svg"?>" width="16" height="16" alt="Add" /> Job Offer apply record
+        </button>
+    </a>
     <div class="company-container">
         <table class="table bg-light-alpha">
             <thead>
